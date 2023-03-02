929e02a75e1c54087e57c0d31d8f1a40c76b6432
preciousimo <preciousimoniakemu@gmail.com>
2023-03-02 23:09:38 +0100

    Create route to products list view

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductCategoryListView, MakerListView
+from .views import ProductCategoryListView, MakerListView, ProductsListView
 
 app_name = "products"
 
 urlpatterns = [
+    path("", ProductsListView.as_view(), name="products-list"),
     path("categories", ProductCategoryListView.as_view(), name="categories-list"),
     path("makers", MakerListView.as_view(), name="makers-list"),
 ]
