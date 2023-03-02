5795025cd32325a2f3ac6a8295df0b7313bc5c73
preciousimo <preciousimoniakemu@gmail.com>
2023-03-02 23:03:56 +0100

    Create route for maker list view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductCategoryListView
+from .views import ProductCategoryListView, MakerListView
 
 app_name = "products"
 
 urlpatterns = [
     path("categories", ProductCategoryListView.as_view(), name="categories-list"),
+    path("makers", MakerListView.as_view(), name="makers-list"),
 ]
