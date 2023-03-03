79953d5077b443b29fcbdad8e2b0379d361ec8c4
preciousimo <preciousimoniakemu@gmail.com>
2023-03-03 16:46:48 +0100

    Create authtokens app

--- ecomobile_backend/settings.py
+++ ecomobile_backend/settings.py
@@ -144,7 +144,6 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        ...
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
