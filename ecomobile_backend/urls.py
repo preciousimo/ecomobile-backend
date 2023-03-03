40e2be4fe71947b4b67b1489ca5329d8b1a48434
preciousimo <preciousimoniakemu@gmail.com>
2023-03-03 18:40:00 +0100

    Available API endpoints

--- ecomobile_backend/urls.py
+++ ecomobile_backend/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import endpoints
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', endpoints, name='endpoints'),
     path("auth/", include("authtokens.urls", namespace="authtokens")),
     path("products/", include("products.urls", namespace="products")),
     path("users/", include("users.urls", namespace="users")),
